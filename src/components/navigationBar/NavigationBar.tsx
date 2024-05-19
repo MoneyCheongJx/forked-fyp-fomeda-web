9c2316a46e45286739bd741dea59bfec6406e5b7
qilin <chungqilin02@gmail.com>
2024-05-20 00:42:49 +0800

    refactor(header): Refactor the code for header component.

--- src/components/navigationBar/NavigationBar.tsx
+++ src/components/navigationBar/NavigationBar.tsx
@@ -1,132 +1,56 @@
 import React from "react";
-import { Image, Button, Dropdown, Space, Divider } from "antd";
-import { DownOutlined, UserOutlined } from "@ant-design/icons";
-import type { MenuProps } from "antd";
-import "../../styles/PrimaryButton.css"
-import "../../styles/Divider.css"
+import {Image, Button, Dropdown, Divider, Avatar, Row} from "antd";
+import {DownOutlined, UserOutlined} from "@ant-design/icons";
+import "../../styles/header.component.css"
+import {MenuProps} from "antd/lib";
+import {Header} from "antd/es/layout/layout";
+import {
+    HEADER_MANAGEMENT_DROPDOWN_LIST_CONSTANTS, HEADER_USER_DROPDOWN_CONSTANTS
+} from "@/utils/header.constants";
 
-import { Header } from "antd/es/layout/layout";
-
-const items: MenuProps["items"] = [
-  {
-    key: "1",
-    label: (
-      <a
-        target="_blank"
-        rel="noopener noreferrer"
-        href="https://www.antgroup.com"
-      >
-        Product Management
-      </a>
-    ),
-  },
-  {
-    key: "2",
-    label: (
-      <a
-        target="_blank"
-        rel="noopener noreferrer"
-        href="https://www.aliyun.com"
-      >
-        Product Category Management
-      </a>
-    ),
-  },
-  {
-    key: "3",
-    label: (
-      <a target="_blank" rel="noopener noreferrer">
-        Product Verification
-      </a>
-    ),
-  },
-  {
-    key: "4",
-    label: (
-      <a target="_blank" rel="noopener noreferrer">
-        Report Management
-      </a>
-    ),
-  },
-  {
-    key: "5",
-    label: (
-      <a target="_blank" rel="noopener noreferrer">
-        Suppliers Management
-      </a>
-    ),
-  },
-  {
-    key: "6",
-    label: (
-      <a target="_blank" rel="noopener noreferrer">
-        Admins Management
-      </a>
-    ),
-  },
-  {
-    key: "7",
-    label: (
-      <a target="_blank" rel="noopener noreferrer">
-        Role Management
-      </a>
-    ),
-  },
-  {
-    key: "8",
-    label: (
-      <a target="_blank" rel="noopener noreferrer">
-        Announcement Management
-      </a>
-    ),
-  },
-  {
-    key: "9",
-    label: (
-      <a target="_blank" rel="noopener noreferrer">
-        Content Management
-      </a>
-    ),
-  },
-];
+const MANAGEMENT_DROPDOWN_LIST: MenuProps["items"] = HEADER_MANAGEMENT_DROPDOWN_LIST_CONSTANTS;
+const PROFILE_DROPDOWN_LIST: MenuProps["items"] = HEADER_USER_DROPDOWN_CONSTANTS;
 
 const NavigationBar = () => {
-  return (
-    <Header
-      style={{
-        position: 'fixed',
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "space-between",
-        width: "100%",
-        backgroundColor: "white",
-      }}
-    >
-      <Image
-        preview={false}
-        width={50}
-        src="/logoFomeda.svg"
-      />
-      <Space>
-        <Button className="primary-button">Home</Button>
-        <Button className="primary-button">Announcement</Button>
-        <Button className="primary-button">Product</Button>
-        <Divider className="divider" type="vertical" />
-        <Dropdown menu={{ items }} arrow>
-          <Button className="primary-button" icon={<DownOutlined />} iconPosition="end">
-            Management
-          </Button>
-        </Dropdown>
-        <Dropdown menu={{ items }}>
-          <Space>
-            <UserOutlined />
-            Name
-            <DownOutlined />
-          </Space>
-        </Dropdown>
-      </Space>
-    </Header>
-  );
+
+    return (
+        <Header
+            style={{
+                position: 'fixed',
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-between",
+                width: "100%",
+                backgroundColor: "white",
+            }}
+        >
+            <Image
+                preview={false}
+                width={50}
+                src="/logoFomeda.svg"
+                alt="fomeda-logo"
+            />
+            <Row style={{alignItems: "center"}}>
+                <Button type="text" className="nav-button">Home</Button>
+                <Button type="text" className="nav-button">Announcement</Button>
+                <Button type="text" className="nav-button">Product</Button>
+                <Divider className="divider" type="vertical"/>
+                <Dropdown menu={{items: MANAGEMENT_DROPDOWN_LIST}}>
+                    <Button type="text" icon={<DownOutlined/>} iconPosition="end">
+                        Management
+                    </Button>
+                </Dropdown>
+                <Row>
+                    <Avatar icon={<UserOutlined/>} className="nav-avatar"/>
+                    <Dropdown menu={{items: PROFILE_DROPDOWN_LIST}}>
+                        <Button type="text" icon={<DownOutlined/>} iconPosition="end">
+                            John Doe
+                        </Button>
+                    </Dropdown>
+                </Row>
+            </Row>
+        </Header>
+    );
 };
 
 export default NavigationBar;
---
+++ src/utils/header.constants.ts
@@ -0,0 +1,16 @@
+export const HEADER_MANAGEMENT_DROPDOWN_LIST_CONSTANTS = [
+    {key: "product_management", label: "Product Management"},
+    {key: "product_category_management", label: "Product Category Management"},
+    {key: "product_verification", label: "Product Verification"},
+    {key: "report_management", label: "Report Management"},
+    {key: "supplier_management", label: "Supplier Management"},
+    {key: "administrator_management", label: "Administrator Management"},
+    {key: "role_management", label: "Role Management"},
+    {key: "announcement_management", label: "Announcement Management"},
+    {key: "content_management", label: "Content Management"},
+]
+
+export const HEADER_USER_DROPDOWN_CONSTANTS = [
+    {key: "logout", label: "Logout"},
+    {key: "manage_profile", label: "Manage Profile"},
+]
