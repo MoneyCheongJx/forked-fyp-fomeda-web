3607f4cae81083b721c2b0c20630b508d2462720
qilin <chungqilin02@gmail.com>
2024-05-20 15:21:41 +0800

    feat(pct): initial commit for product category.

---
+++ src/app/category-management/page.tsx
@@ -0,0 +1,39 @@
+"use client"
+
+import React from "react";
+import PageLayout from '@/app/page';
+import {Breadcrumb, Button, Col, Input, Row, Tabs} from "antd";
+import {CATEGORY_TAB_CONSTANTS} from "@/utils/category.constant";
+import {PlusOutlined, SearchOutlined} from "@ant-design/icons";
+
+const categoryChildren = (
+    <Row style={{ justifyContent: "space-between" }}>
+        <Col span={8}>
+            <Input placeholder="category" prefix={<SearchOutlined />} size="large" ></Input>
+        </Col>
+        <Button type="primary" icon={<PlusOutlined />} iconPosition="start" >Add Category</Button>
+    </Row>
+)
+const generalChildren = (<h1>Tab2</h1>)
+
+const CATEGORY_TAB_CONSTANT = CATEGORY_TAB_CONSTANTS.map((item) => ({
+    key: item.key,
+    label: item.label,
+    children: item.key === "category" ? categoryChildren : generalChildren,
+}));
+
+const onChange = (key: string) => {
+    console.log(key);
+};
+
+const ProductPage = () => {
+    return (
+        <PageLayout>
+            <Breadcrumb items={[{title: 'Product Category', href: '/category-management'}]} />
+            <h1>Product Category</h1>
+            <Tabs defaultActiveKey="category" items={CATEGORY_TAB_CONSTANT} onChange={onChange} size="small"></Tabs>
+        </PageLayout>
+    );
+};
+
+export default ProductPage;
