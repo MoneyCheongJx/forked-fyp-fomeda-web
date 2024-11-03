51c7e3c240d0b9827a73201458bcea10dfd6b1b0
MoneyCheongJx <moneycheongjx2002@gmail.com>
2024-11-03 23:13:18 +0800

    fix(auth) fix auth routing logic page

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,37 +5,44 @@ import { jwtDecode } from "jwt-decode";
 
 export function middleware(req: NextRequest) {
     const { pathname } = req.nextUrl;
-    const isProtected = pathname.startsWith('/management');
+    const isManagementNavigation = pathname.startsWith('/management');
     const isProfileNavigation = pathname.startsWith('/management/profile');
+    const isResetPasswordNavigation = pathname.startsWith('/reset-password');
+    const isResetVerified = req.cookies.get('isResetVerified')?.value;
     const token = req.cookies.get('token')?.value;
 
-    if (isProtected && !token) {
-        return NextResponse.redirect(new URL('/content', req.url));
-    }
+    if (isResetPasswordNavigation) {
+        if (isResetVerified !== 'true') {
+            return NextResponse.redirect(new URL('/forget-password', req.url));
+        }
+    } else if (isManagementNavigation) {
+        if (!token)
+            return NextResponse.redirect(new URL('/content', req.url));
 
-    let decoded: any;
-    try {
-        decoded = jwtDecode<CustomJwtPayload>(token as string)
-    } catch (error) {
-        return NextResponse.redirect(new URL('/content', req.url));
-    }
-    
-    // skip module checking for profile navigation but requires checking for token
-    if (isProfileNavigation) {
+        let decoded: any;
+        try {
+            decoded = jwtDecode<CustomJwtPayload>(token as string)
+        } catch (error) {
+            return NextResponse.redirect(new URL('/content', req.url));
+        }
+
+        // skip module checking for profile navigation but requires checking for token
+        if (isProfileNavigation) {
+            return NextResponse.next();
+        }
+
+        const userModules = decoded?.modules || [];
+        const moduleFromPath = pathname.split('/')[2].replace(/-/g, '_');;
+        const hasAccess = userModules.some((module: string | string[]) => module.includes(moduleFromPath));
+
+        if (!hasAccess) {
+            return NextResponse.redirect(new URL('/content', req.url));
+        }
+
         return NextResponse.next();
     }
-
-    const userModules = decoded?.modules || [];
-    const moduleFromPath = pathname.split('/')[2].replace(/-/g, '_');;
-    const hasAccess = userModules.some((module: string | string[]) => module.includes(moduleFromPath));
-
-    if (!hasAccess) {
-        return NextResponse.redirect(new URL('/content', req.url));
-    }
-
-    return NextResponse.next();
 }
 
 export const config = {
-    matcher: ['/management/:path*'],
+    matcher: ['/management/:path*', '/reset-password/:path*'],
 };
