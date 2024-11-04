baf5c71be2d7da1b28c34d198378aa26652b5b28
MoneyCheongJx <moneycheongjx2002@gmail.com>
2024-11-04 09:00:09 +0800

    fix(middleware.ts) add delete account routing proctection

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,16 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
-import { CustomJwtPayload } from "@/models/jwt.model";
-import { jwtDecode } from "jwt-decode";
+import {NextResponse} from 'next/server';
+import type {NextRequest} from 'next/server';
+import {CustomJwtPayload} from "@/models/jwt.model";
+import {jwtDecode} from "jwt-decode";
 
 export function middleware(req: NextRequest) {
-    const { pathname } = req.nextUrl;
+    const {pathname} = req.nextUrl;
     const isManagementNavigation = pathname.startsWith('/management');
     const isProfileNavigation = pathname.startsWith('/management/profile');
+    const isDeleteAccountNavigation = pathname.startsWith('/management/profile/delete-account/');
     const isResetPasswordNavigation = pathname.startsWith('/reset-password');
     const isResetVerified = req.cookies.get('isResetVerified')?.value;
+    const isDeleteVerified = req.cookies.get('isDeleteVerified')?.value;
     const token = req.cookies.get('token')?.value;
 
     if (isResetPasswordNavigation) {
@@ -28,6 +30,11 @@ export function middleware(req: NextRequest) {
 
         // skip module checking for profile navigation but requires checking for token
         if (isProfileNavigation) {
+            if (isDeleteAccountNavigation) {
+                if (isDeleteVerified !== 'true') {
+                    return NextResponse.redirect(new URL('/management/profile', req.url));
+                }
+            }
             return NextResponse.next();
         }
 
